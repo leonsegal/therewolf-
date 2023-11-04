493b550c206b80ef1ed265031d6cfa1c41c6c33d
leon <leonsegal1@gmail.com>
2023-11-04 13:27:20 +0000

    listen for connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 let express = require("express");
 let app = express();
-let http = require("http");
 let cors = require("cors");
+let http = require("http");
+let { Server } = require("socket.io");
 
-app.use(cors());
+app.use(cors()); // middleware
 
 let server = http.createServer(app);
 let port = process.env.PORT || 4000;
+
+let io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`); // deleteme
+});
 
 server.listen(port, () => `Server is running on port ${port}`);
