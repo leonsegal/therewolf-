3d1e3d65afb4547e7274d18d9563e5a8d3447e2d
leon <leonsegal1@gmail.com>
2023-11-04 14:46:41 +0000

    emit messages on connection

--- server/index.js
+++ server/index.js
@@ -16,8 +16,35 @@ let io = new Server(server, {
   },
 });
 
+let chatBot = "ChatBot";
+let chatRoom = "";
+let allUsers = [];
+
 io.on("connection", (socket) => {
-  console.log(`User connected ${socket.id}`); // deleteme
+  let createdTime = new Date().toLocaleTimeString();
+
+  socket.on("join_room", (data) => {
+    let { userName, room } = data;
+    socket.join(room);
+
+    socket.to(room).emit("receive_message", {
+      message: `${userName} has joined the chat room`,
+      userName: chatBot,
+      createdTime,
+    });
+
+    socket.emit("receive_message", {
+      message: `Welcome ${userName}`,
+      userName: chatBot,
+      createdTime,
+    });
+
+    chatRoom = room;
+    allUsers.push({ id: socket.id, userName, room });
+    let chatRoomUsers = allUsers.filter((user) => user.room === room);
+    socket.to(room).emit("chat_room_users", chatRoomUsers);
+    socket.emit("chat_room_users", chatRoomUsers);
+  });
 });
 
 server.listen(port, () => `Server is running on port ${port}`);
